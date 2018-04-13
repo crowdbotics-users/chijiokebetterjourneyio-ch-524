266642708cdad265e3b6b1837a40f8d512018e64
KamyFC <kamyfc@KamyFCs-MacBook-Pro.local>
2018-04-13 11:35:19 +0530

    Fixing scripts during Fastlane invocation

--- swift/crowdbotics/fastlaneScript.sh
+++ swift/crowdbotics/fastlaneScript.sh
@@ -2,5 +2,7 @@
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export FASTLANE_PASSWORD=Genghis109
+export PATH="$HOME/.fastlane/bin:$PATH"
 
-fastlane do_everything
+/Library/Ruby/Gems/2.3.0/gems/fastlane-2.90.0/bin/fastlane do_everything
